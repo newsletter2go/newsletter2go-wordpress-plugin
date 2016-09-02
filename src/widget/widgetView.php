c6625aa9aced4a2be47b5d3803b12d9509b8133d
Vojin Janevski <vojin.janevski@logeecom.com>
2016-09-02 16:53:31 +0200

    [feature/SI-486]: Changed url of the script to staging.

--- src/widget/widgetView.php
+++ src/widget/widgetView.php
@@ -5,9 +5,9 @@
             e.Newsletter2GoTrackingObject = r, e[r] = e[r] || function () {
                     (e[r].q = e[r].q || []).push(arguments)
                 }, e[r].l = 1 * new Date, a = t.createElement(n), i = t.getElementsByTagName(n)[0], a.async = 1, a.src = c, i.parentNode.insertBefore(a, i)
-        }(window, document, "script", "//static.newsletter2go.com/utils.js", "n2g");
+        }(window, document, "script", "//static-staging.newsletter2go.com/utils.js", "n2g");
         n2g('create', '<?=$formUniqueCode?>');
         n2g(<?= $n2gParams?>);
-        
+
     });
 </script>
