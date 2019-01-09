3af1cee7e1386e1a1b096c99ff6f15d8b3927c2f
Ahmad2 <ahmad@newsletter2go.com>
2019-01-09 12:10:52 +0100

    enhance code using eval()

--- src/widget/widgetView.js
+++ src/widget/widgetView.js
@@ -6,17 +6,8 @@
 n2g("create", formUniqueCode);
 
 if (uniqueId && !popup) {
-    n2gParams.push(uniqueId);
-    for (let i in n2gParams) {
-        if (n2gParams[i] == "null") {
-            n2gParams[i] = null;
-        }
-        if (n2gParams[i] != null && n2gParams[i] != "" && n2gParams[i].slice(0, 1) == "'" && n2gParams[i].slice(-1) == "'") {
-            n2gParams[i] = n2gParams[i].slice(1, -1);
-        }
-    }
-
-    n2g.apply(this, n2gParams);
-} else {
-    n2g.apply(this, n2gParams);
+    n2gParams.push("'" + uniqueId+ "'");
 }
+let n2gFun = "n2g(" + n2gParams.join(", ") + ")";
+eval(n2gFun);
+
